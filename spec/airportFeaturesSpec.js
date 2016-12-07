3b190ba386bfe334806e96cdb2b7b3fee8d85c01
TudorTacal <tudortacal@gmail.com>
2016-12-07 17:47:55 +0000

    User story 3

--- spec/airportFeaturesSpec.js
+++ spec/airportFeaturesSpec.js
@@ -1,42 +1,36 @@
 describe('Features Tests', function () {
   var airport;
   var plane;
-  var weather;
 
   beforeEach(function () {
     airport = new Airport();
     plane = new Plane();
-    plane.land(airport);
+    weather = {
+      isStormy: function () {
+      }
+    }
+
   });
 
   describe('User story 1', function (){
 
     it('a plane will land at an airport', function () {
+      plane.land(airport);
       expect(airport.planes.length).toEqual(1);
     });
   });
 
   describe('User story 2', function (){
     it('a plane will take off from an airport', function(){
+      plane.land(airport);
       airport.takeOff(plane);
       expect(airport.planes.length).toEqual(0);
     });
-  });
 
-  describe('User story 2', function (){
-    var weather;
-    beforeEach(function() {
-      // weather = jasmine.createSpyObject('weather',['isStormy']);
-      weather = {
-        isStormy:  function() {
-          return true;
-        }
-      };
-      spyOn(weather, 'isStormy');
-    });
     it('prevents plane from taking off when wheather is stormy', function(){
-      weather.isStormy;
-      expect(airport.land(plane)).toThrowError("Cannot land plane: weather is stormy.");
+      spyOn(weather, 'isStormy').and.returnValue(true);
+      expect(function () {
+        airport.land(plane);}).toThrow("Cannot land plane: weather is stormy.");
+      });
     });
   });
-});
--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,22 +1,31 @@
 describe('Airport', function(){
   var airport;
   var plane;
+
   beforeEach(function(){
     airport = new Airport();
     plane = jasmine.createSpy('plane');
+    weather = {
+      isStormy: function () {
+      }
+    }
   });
   describe('#land', function(){
     it('should land a plane', function(){
       airport.land(plane);
       expect(airport.planes.length).toEqual(1);
     });
-  });
-  describe('#takeOff', function (){
-    it('should take off a plane', function (){
-      airport.takeOff(plane);
-      expect(airport.planes.length).toEqual(0);
+    it('should not land a plane when the weather is stormy',function () {
+      spyOn(weather, 'isStormy').and.returnValue(true);
+      expect(function () {
+        airport.land(plane);}).toThrow("Cannot land plane: weather is stormy.");
+      });
     });
-  });
-  
+    describe('#takeOff', function (){
+      it('should take off a plane', function (){
+        airport.takeOff(plane);
+        expect(airport.planes.length).toEqual(0);
+      });
+    });
 
-});
+  });

fa53a95d8bd9d9ba010112954970de0ff905f842
keomony khun <keomony@gmail.com>
2016-12-07 16:39:25 +0000

    random stormy weather

--- spec/weatherSpec.js
+++ spec/weatherSpec.js
@@ -1,7 +1,18 @@
 describe('Weather',function(){
-  var weather = new Weather();
-  it('should return true when the weather is stormy', function () {
+  var sunnyWeather;
+  var stormyWeather;
+  beforeEach(function(){
+    sunnyWeather = new Weather();
+    stormyWeather = new Weather();
+  });
 
-    expect(weather.isStormy).toEqual(true);
+  it('should return true when the weather is stormy', function () {
+    spyOn(Math, "random").and.returnValue(0.5);
+    expect(stormyWeather.isStormy()).toBeTruthy();
+  });
+
+  it('should return false when the weather is sunny', function(){
+    spyOn(Math, "random").and.returnValue(0.9);
+    expect(sunnyWeather.isStormy()).toBeFalsy();
   });
 });
